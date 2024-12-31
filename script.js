3ac9e7b4cf182ff8040468277f2aa26832ac3b29
Julia <julia@Julias-MacBook-Pro.local>
2024-12-31 16:20:25 -0600

    movie colors

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 
+//SCREEN SIZING
 let screenWidth, screenHeight;
-
-// get screen size
-function updateScreenSize() {
+function updateScreenSize() {// get screen size
     screenWidth = window.innerWidth; 
     screenHeight = window.innerHeight; 
 }
+window.onload = updateScreenSize;//update screen size is called when page is loaded. 
+window.onresize = updateScreenSize;//update screen when window is resized
 
-//update screen size is called when page is loaded. 
-window.onload = updateScreenSize;
 
-//update screen when window is resized
-window.onresize = updateScreenSize;
+
+//MOVIE CODE
+const movieItems = document.querySelectorAll('.movie-list li');
+const genreText = document.querySelector('.genre-text');
+
+movieItems.forEach((item) => {
+    item.addEventListener('mouseenter', () => { //genre from the data attribute
+        const genre = item.getAttribute('data-genre');
+        genreText.textContent = genre; //display text
+    });
+    item.addEventListener('mouseleave', () => {
+        genreText.textContent = ''; // clear genre display
+    });
+});
